1d31dff7fa0e023e1de0b4c05469faaf9bc30a0d
LeoAChaves <leochaves2121@gmail.com>
2022-04-01 14:50:08 -0300

    ListaJS arrowFunctions

---
+++ Aula1/ListaJS/arrowFunctions.js
@@ -0,0 +1,15 @@
+upperName = (name) => {
+  return name.toUpperCase();
+};
+
+myFunction = (p1, p2) => {
+  return p1 * p2;
+};
+
+toCelsius = (fah) => {
+  return (5 / 9) * (fah - 32);
+};
+
+console.log(upperName("Leo Chaves"));
+console.log(myFunction(21, 21));
+console.log(toCelsius(212));
